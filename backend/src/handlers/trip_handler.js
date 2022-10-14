b96846e885d5298125ef3305086dc8e68c5654a2
r2go-beep <hans@hummelusa.com>
2022-10-14 12:51:45 -0500

    Trip API Implementation

--- backend/src/handlers/trip_handler.js
+++ backend/src/handlers/trip_handler.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const { TRIP_STATUS, RATING_BOUNDS, DEFAULT_TRIP} = require('../shared/definitions')
 
 function getTripHandler(TripController) {
     const router = express.Router()
+
+    /* =============================   Trips Get Info   =============================*/
     
-    /**
-        * @api {get} api/trips/:tripID      Get the information about a particular trip  
-        * @apiName GetTrip
+    /** 
+        * @api {get} api/trips/id/:tripID      Get the information about a particular trip  
+        * @apiName GetTripByID
         * @apiGroup Trip
+        * @param tripID                     The tripID
         * @apiSuccess {data}                Get info with given tripID
     */
-    router.get('/:tripID', async function(req, res, next) {
+    router.get('/id/:tripID', async function(req, res, next) {
         try {
             const tripID = req.params.tripID
             if (tripID){
@@ -21,6 +25,317 @@ function getTripHandler(TripController) {
             }
         }catch(err) {
             const errMessage = "Error while retrieving trip: " + err.message
+            res.status(404)
+            res.send(errMessage)
+            next(err);
+        }
+    })
+
+    /**
+        * @api {get} api/trips/all             Get all trips
+        * @apiName GetAllTrips
+        * @apiGroup Trip
+        * @apiSuccess {data}                All trips
+    */
+    router.get('/all', async function(req, res, next) {
+        try {
+            const data = await TripController.getAllTrips()
+            res.json(data)
+
+        }catch(err) {
+            const errMessage = "Error while retrieving trips: " + err.message
+            res.status(404)
+            res.send(errMessage)
+            next(err);
+        }
+    })
+
+    /**
+        * @api {get} api/trips/driver/:driverID       Get all trips with driverID
+        * @apiName GetTripsWithDriver
+        * @apiGroup Trip
+        * @apiSuccess {data}                          Trip list data
+    */
+    router.get('/driver/:driverID', async function(req, res, next) {
+        try {
+            const driverID = req.params.driverID
+            if (driverID){
+                const data = await TripController.getTripWithDriver(driverID)
+                res.json(data)
+            } else {
+                res.status(400)
+                res.send("Check request parameters/body")
+            }
+        }catch(err) {
+            const errMessage = "Error while retrieving trips: " + err.message
+            res.status(404)
+            res.send(errMessage)
+            next(err);
+        }
+    })
+
+    /**
+        * @api {get} api/trips/passenger/:passengerID       Get all trips with passengerID
+        * @apiName GetTripsWithPassenger
+        * @apiGroup Trip
+        * @apiSuccess {data}                                Trip list data
+    */
+    router.get('/passenger/:passengerID', async function(req, res, next) {
+        try {
+            const passengerID = req.params.passengerID
+            if (passengerID){
+                const data = await TripController.getTripWithPassenger(passengerID)
+                res.json(data)
+            } else {
+                res.status(400)
+                res.send("Check request parameters/body")
+            }
+        }catch(err) {
+            const errMessage = "Error while retrieving trips: " + err.message
+            res.status(404)
+            res.send(errMessage)
+            next(err);
+        }
+    })
+
+    /* =============================   Trip Progress Manipulation   =============================*/
+
+    /**
+        * @api {get} api/trips/ride/next             Get next trip info of oldest trip in queue
+        * @apiName GetNextTrip
+        * @apiGroup Trip
+        * @apiSuccess {data}                         Gets data of the next trip in queue
+    */
+    router.get('/ride/next', async function(req, res, next) {
+        try {
+            const data = await TripController.getNextTrip()
+            res.json(data)
+        }catch(err) {
+            const errMessage = "Error while retrieving trips: " + err.message
+            res.status(404)
+            res.send(errMessage)
+            next(err);
+        }
+    })
+
+    /**
+        * @api {post} api/trips/ride/start/:passengerID           Start a trip with passenger
+        * @apiName StartTrip
+        * @apiGroup Trip
+        * @apiBody  GeoLocation                             GeoLocation of the start trip in JSON format 
+        * The latitude of this GeoPoint in the range [-90, 90].
+        * The longitude of this GeoPoint in the range [-180, 180].
+        * @apiSuccess {data}                                tripID
+    */
+    router.post('/ride/start/:passengerID', async function(req, res, next) {
+        try {
+            const passengerID = req.params.passengerID
+            const startLocation = req.body
+        
+            if ( 
+                passengerID && startLocation.hasOwnProperty('latitude') && startLocation.hasOwnProperty('longitude') &&
+                startLocation.latitude >= -90 && startLocation.latitude <= 90 &&
+                startLocation.longitude >= -180 && startLocation.longitude <= 180
+            ){
+                const data = await TripController.createTrip(passengerID, startLocation)//replace this body geo location
+                res.json(data)
+            } else {
+                res.status(400)
+                res.send("Check request parameters/body")
+            }
+        }catch(err) {
+            const errMessage = "Error while starting trip: " + err.message
+            res.status(404)
+            res.send(errMessage)
+            next(err);
+        }
+    })
+
+    /**
+        * @api {put} api/trips/ride/set_driver/:tripID&:driverID             Sets the trip with a driver and updated its status
+        * @apiName SetTripDriver
+        * @apiGroup Trip
+        * @apiSuccess {data}                                                Returns whether this op was successful
+    */
+    router.put('/ride/set_driver/:tripID&:driverID', async function(req, res, next) {
+        try {
+            const tripID = req.params.tripID
+            const driverID = req.params.driverID
+            if (driverID && tripID){
+                const data = await TripController.setDriverTrip(tripID, driverID)
+                res.json(data)
+            } else {
+                res.status(400)
+                res.send("Check request parameters/body")
+            }
+        }catch(err) {
+            const errMessage = "Error while setting trip driver: " + err.message
+            res.status(404)
+            res.send(errMessage)
+            next(err);
+        }
+    })
+
+    /**
+        * @api {put} api/trips/ride/update_status/:tripID&:status                    Rates the Driver performance for the trip
+        * @apiName RateDriverPerformance
+        * @apiGroup Trip
+        * @apiSuccess {data}                                                Returns weather this op was successful
+    */
+    router.put('/ride/update_status/:tripID&:status', async function(req, res, next) {
+        try {
+            const tripID = req.params.tripID
+            const status = req.params.status
+
+            if (tripID && status && TRIP_STATUS.hasOwnProperty(status)) {
+                const data = await TripController.updateTripStatus(tripID, status)
+                res.json(data)
+            } else {
+                res.status(400)
+                res.send("Check request parameters/body")
+            }
+        }catch(err) {
+            const errMessage = "Error while updating the trips status: " + err.message
+            res.status(404)
+            res.send(errMessage)
+            next(err);
+        }
+    })
+
+    /**
+        * @api {put} api/trips/ride/end/:tripID                             Ends the Trip
+        * @apiName EndTrip
+        * @apiGroup Trip
+        * @apiSuccess {data}                                                Returns weather this op was successful
+    */
+    router.put('/ride/end/:tripID', async function(req, res, next) {
+        try {
+            const tripID = req.params.tripID
+            const endLocation = req.body
+
+            if (
+                tripID && endLocation.hasOwnProperty('latitude') && endLocation.hasOwnProperty('longitude') &&
+                endLocation.latitude >= -90 && endLocation.latitude <= 90 &&
+                endLocation.longitude >= -180 && endLocation.longitude <= 180
+            ) {
+                const data = await TripController.updateTripStatus(tripID, endLocation)
+                res.json(data)
+            } else {
+                res.status(400)
+                res.send("Check request parameters/body")
+            }
+        }catch(err) {
+            const errMessage = "Error while Ending Trip: " + err.message
+            res.status(404)
+            res.send(errMessage)
+            next(err);
+        }
+    })
+
+    /**
+        * @api {put} api/trips/ride/rate/driver/:tripID                     Rates the Driver performance for the trip
+        * @apiName RateDriverPerformance
+        * @apiGroup Trip
+        * @apiSuccess {data}                                                Returns weather this op was successful
+    */
+    router.put('/ride/rate/driver/:tripID&:rating', async function(req, res, next) {
+        try {
+            const tripID = req.params.tripID
+            const rating = req.params.rating
+
+            if (tripID && rating && rating >= RATING_BOUNDS.lower_bound && rating <= RATING_BOUNDS.upper_bound ) {
+                const data = await TripController.setTripDriverRating(tripID, rating)
+                res.json(data)
+            } else {
+                res.status(400)
+                res.send("Check request parameters/body")
+            }
+        }catch(err) {
+            const errMessage = "Error while setting the Driver rating: " + err.message
+            res.status(404)
+            res.send(errMessage)
+            next(err);
+        }
+    })
+
+        /**
+        * @api {put} api/trips/ride/rate/passenger/:tripID                  Rates the Passenger performance for the trip
+        * @apiName RateDriverPerformance
+        * @apiGroup Trip
+        * @apiSuccess {data}                                                Returns weather this op was successful
+    */
+         router.put('/ride/rate/passenger/:tripID&:rating', async function(req, res, next) {
+            try {
+                const tripID = req.params.tripID
+                const rating = req.params.rating
+    
+                if (tripID && rating && rating >= RATING_BOUNDS.lower_bound && rating <= RATING_BOUNDS.upper_bound ) {
+                    const data = await TripController.setTripPassengerRating(tripID, rating)
+                    res.json(data)
+                } else {
+                    res.status(400)
+                    res.send("Check request parameters/body")
+                }
+            }catch(err) {
+                const errMessage = "Error while setting the Passenger rating: " + err.message
+                res.status(404)
+                res.send(errMessage)
+                next(err);
+            }
+        })
+
+    /* =============================   Trip Admin Manipulation   =============================*/
+
+    /**
+        * @api {delete} api/trips/delete/:tripID           Delete a trip with tripID
+        * @apiName DeleteTrip
+        * @apiGroup Trip
+        * @apiSuccess {data}                               Success/Failure
+    */
+    router.delete('/delete/:tripID', async function(req, res, next) {
+        try {
+            const tripID = req.params.tripID
+            if (tripID){
+                const data = await TripController.deleteTrip(tripID)
+                res.json(data)
+            } else {
+                res.status(400)
+                res.send("Check request parameters/body")
+            }
+        }catch(err) {
+            const errMessage = "Error while deleting trip: " + err.message
+            res.status(404)
+            res.send(errMessage)
+            next(err);
+        }
+    })
+
+    /**
+        * @api {delete} api/trips/delete/:tripID           Delete a trip with tripID
+        * @apiName DeleteTrip
+        * @apiGroup Trip
+        * @apiSuccess {data}                               Success/Failure
+    */
+    router.put('/manipulate/:tripID', async function(req, res, next) {
+        try {
+            const tripID = req.params.tripID
+            const attr = req.body
+
+            is_valid_attr = true
+            Object.keys(myObject).forEach(attribute => {
+                if (is_valid_attr) is_valid_attr = DEFAULT_TRIP.hasOwnProperty(attribute)
+                if (is_valid_attr) is_valid_attr = typeof(DEFAULT_TRIP.attribute) == typeof(attribute)
+            });
+
+            if (tripID && attr && is_valid_attr){
+                const data = await TripController.deleteTrip(tripID)
+                res.json(data)
+            } else {
+                res.status(400)
+                res.send("Check request parameters/body")
+            }
+        }catch(err) {
+            const errMessage = "Error while deleting trip: " + err.message
             res.status(404)
             res.send(errMessage)
             next(err);
