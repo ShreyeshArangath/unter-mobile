6ce05e29e254edffe97362a64a5610c1de035ac4
Devin James <devsch1@gmail.com>
2022-10-22 14:32:20 -0500

    Squashed commit of the following:
    commit ebc7de5a3dbcf742594c02239d9051458da45d0d
    Merge: be46e63 6b26537
    Author: Shreyesh <imshreyesh17@gmail.com>
    Date:   Sat Oct 22 14:12:42 2022 -0500
    
        Merge pull request #19 from ShreyeshArangath/shreyesh
    
        added google map and google places autocomplete api support
    
    commit 6b26537594767d55238482c5a1cbf3ee6c040946
    Author: Shreyesh Arangath <shreyesh@Shreyeshs-MacBook-Pro.local>
    Date:   Fri Oct 21 09:24:36 2022 -0500
    
        removed duplication of the default state value for region
    
    commit c6b807cf648eeb3c5be40d53fcf1396aadaf33f2
    Author: Shreyesh Arangath <shreyesh@Shreyeshs-MacBook-Pro.local>
    Date:   Thu Oct 20 21:27:49 2022 -0500
    
        updated gitignore
    
    commit 9fcacea771e2e9a044360e7c6f6aa49f85fb2295
    Author: Shreyesh Arangath <shreyesh@Shreyeshs-MacBook-Pro.local>
    Date:   Thu Oct 20 21:25:11 2022 -0500
    
        added google map and google places autocomplete api support
    
    commit be46e63fa95f597b4826739ea05e60aa1ede1157
    Merge: 7f12a64 b96846e
    Author: TachyonicSpace <40440025+TachyonicSpace@users.noreply.github.com>
    Date:   Mon Oct 17 15:10:52 2022 -0500
    
        Merge pull request #15 from r2go-beep/main
    
        Trip API Implementation
    
    commit b96846e885d5298125ef3305086dc8e68c5654a2
    Author: r2go-beep <hans@hummelusa.com>
    Date:   Fri Oct 14 12:51:45 2022 -0500
    
        Trip API Implementation

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -9,11 +9,14 @@ import {
   extendTheme,
   VStack,
   Box,
+  useSafeArea,
 } from 'native-base';
-import { useState } from 'react';
 import { Dimensions } from 'react-native';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+import {GOOGLE_MAPS_API_KEY} from '@env';
 const baseUrl = 'http://10.172.85.36:9001/api/trips/id/Trip'
+
 
 
 //importing the code for the Other UI's
@@ -22,24 +25,54 @@ import { Admin } from './Users/Admin';
 import { Driver } from './Users/Driver';
 import { UsersClass } from './Users/Shared';
 import { UIUser } from './Components/UIUser';
+import GoogleMap from './components/google_map';
+import GoogleMapSearch from './components/google_map_search';
+import { SafeAreaView } from 'react-native';
+import * as Location from 'expo-location';
 
-//the main function of the program, like main() in C.
-export default () => {
 
-    //allow us to have a boolean value for what views we are looking at
-    const [showPassenger, setPassenger] = useState(false);
-    const [showAdmin, setAdmin] = useState(false);
-    const [showDriver, setDriver] = useState(false);
+export default function App() {
+  const [location, setLocation] = useState(null);
+  const [locationErrMsg, setLocationErrMsg] = useState(null);
+  //allow us to have a boolean value for what views we are looking at
+  const [showPassenger, setPassenger] = useState(false);
+  const [showAdmin, setAdmin] = useState(false);
+  const [showDriver, setDriver] = useState(false);
 
+  const [region, setRegion] = useState({
+      latitude: 37.78825, 
+      longitude: -122.4324 
+  })
+
+  const getUserLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setLocationErrMsg('Permission to access location was denied');
+      return;
+    }
+  
+    await Location.getCurrentPositionAsync({}).then((location) => {
+      setRegion({
+        latitude: location.coords.latitude, 
+        longitude: location.coords.longitude
+      });
+      console.log(region)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+
+  useEffect(() => { getUserLocation()}, [])
     //stores these in an object so we can pass them by reference 
     //to the other UI functions
     let uiPassenger = new UIUser(showPassenger, setPassenger, new UsersClass("Passenger"));
     let uiAdmin = new UIUser(showAdmin, setAdmin, new UsersClass("Admin"));
     let uiDriver = new UIUser(showDriver, setDriver, new UsersClass("Driver"));
 
-    //used to store the HTML Element object that stores our UI code
-    let activeCode;//the code we are returning to run 
+  //used to store the HTML Element object that stores our UI code
+  let activeCode;//the code we are returning to run 
 
+  
 
     //do these functions if the boolean saying to do them is enabled
     if (showPassenger)
---
+++ mobile-app/api/live_location.js
@@ -0,0 +1,15 @@
+export default class LiveLocation {
+    constructor(firebaseConfig) {
+        this._firebase = firebaseConfig 
+    }
+
+    //TODO: Create a trip document to the Firebase Realtime DB
+    async createLiveLocationDoc() {
+
+    }
+
+    //TODO: Update a trip document to the Firebase Realtime DB
+    async updateLiveLocationDoc() {
+
+    }
+}
