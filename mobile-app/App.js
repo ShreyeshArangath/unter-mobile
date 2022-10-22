0118615a5fae364679fd801896d9a2dc78d700cc
Devin James <devsch1@gmail.com>
2022-10-22 15:12:09 -0500

    Merged from the google maps API

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -28,8 +28,6 @@ import { Admin } from './Users/Admin';
 import { Driver } from './Users/Driver';
 import { UsersClass } from './Users/Shared';
 import { UIUser } from './Components/UIUser';
-import GoogleMap from './components/google_map';
-import GoogleMapSearch from './components/google_map_search';
 import { SafeAreaView } from 'react-native';
 import * as Location from 'expo-location';
 
