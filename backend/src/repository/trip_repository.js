9950b369690bb4bcca55f265a33b9df1e12a1125
Devin James <devsch1@gmail.com>
2022-10-26 09:47:41 -0500

    improved console logging on trip backend

--- backend/src/repository/trip_repository.js
+++ backend/src/repository/trip_repository.js
@@ -5,13 +5,23 @@ class TripRepository {
     constructor(firebaseBundle) {
         this._isLogQuery = true
         this._db = firebaseBundle.cloudFirestore
+
+        this.successColor = '\x1b[34m%s\x1b[0m';//sets the color to blue, and then resets the console to normal
+        this.failColor = '\x1b[31m%s\x1b[0m';//sets the color to res, then resets the console to normal
     }
 
     async getTrip(tripID) {
-        if (this._isLogQuery) console.log('Getting Trip with tripID: ' + tripID)
-
+        
         const querySnapshot = await getDocs(query(collection(this._db, 'Trips'),
-            where("__name__", "==", tripID)))
+        where("__name__", "==", tripID)))
+        
+        if(querySnapshot.size == 0)
+        {
+            if (this._isLogQuery) console.log(this.failColor, "error trying to retrieve trip with id: " + tripID);
+        }
+        else
+            if (this._isLogQuery) console.info(this.successColor, 'Getting Trip with tripID: ' + tripID)
+
         var jsonResult = {}
         querySnapshot.forEach(doc => {
             jsonResult[doc.id] = doc.data()
@@ -20,9 +30,16 @@ class TripRepository {
     }
 
     async getAllTrips() {
-        if (this._isLogQuery) console.log('Getting All Trips')
-
+        
         const querySnapshot = await getDocs(collection(this._db, 'Trips'));
+        if(querySnapshot.size == 0)
+        {
+            if (this._isLogQuery) console.log(this.failColor, "getting all trips returned empty JSON");
+        }
+        else
+            if (this._isLogQuery) console.info(this.successColor, 'Getting All Trips')
+
+
         var jsonResult = {}
         querySnapshot.forEach(doc => {
             jsonResult[doc.id] = doc.data()
@@ -31,10 +48,17 @@ class TripRepository {
     }
 
     async getTripsWithDriverID(driverID) {
-        if (this._isLogQuery) console.log('Getting Trip with driverID: ' + driverID)
-
+        
         const querySnapshot = await getDocs(query(collection(this._db, 'Trips'),
-            where("driverID", "==", driverID)))
+        where("driverID", "==", driverID)))
+        
+        if(querySnapshot.size == 0)
+        {
+            if (this._isLogQuery) console.log(this.failColor, "error trying to retrieve trip with driver id: " + driverID);
+        }
+        else
+            if (this._isLogQuery) console.info(this.successColor, 'Getting Trip with driverID: ' + driverID)
+
         var jsonResult = {}
         querySnapshot.forEach(doc => {
             jsonResult[doc.id] = doc.data()
@@ -43,10 +67,17 @@ class TripRepository {
     }
 
     async getTripsWithPassengerID(passID) {
-        if (this._isLogQuery) console.log('Getting Trip with passengerID: ' + passID)
 
         const querySnapshot = await getDocs(query(collection(this._db, 'Trips'),
             where("passID", "==", passID)))
+        
+        if(querySnapshot.size == 0)
+        {
+            if (this._isLogQuery) console.log(this.failColor, "error trying to retrieve trip with passenger id: " + driverID);
+        }
+        else
+            if (this._isLogQuery) console.info(this.successColor, 'Getting Trip with passengerID: ' + driverID)
+
         var jsonResult = {}
         querySnapshot.forEach(doc => {
             jsonResult[doc.id] = doc.data()
@@ -55,15 +86,22 @@ class TripRepository {
     }
 
     async getNextTrip(trip_status) {
-        if (this._isLogQuery) console.log('Getting Longest Waiting Trip')
-
+        
         const querySnapshot = await getDocs(
-                query(  collection(this._db, 'Trips'),
-                        where("status", "==", trip_status),
-                        orderBy("startTime", "asc"),
-                        limit(1)
-                )
+            query(  collection(this._db, 'Trips'),
+            where("status", "==", trip_status),
+            orderBy("startTime", "asc"),
+            limit(1)
             )
+            );
+            
+        if(querySnapshot.size == 0)
+        {
+            if (this._isLogQuery) console.log(this.failColor, "longest waiting trip returned nothing");
+        }
+        else
+            if (this._isLogQuery) console.info(this.successColor, 'Getting Longest Waiting Trip')
+
         var jsonResult = {}
         querySnapshot.forEach(doc => {
             jsonResult[doc.id] = doc.data()
@@ -71,8 +109,8 @@ class TripRepository {
         return jsonResult
     }
 
-    async createTrip(tripInfo) {
-        if (this._isLogQuery) console.log('Creating Trip with passengerID: ' + tripInfo.passID)
+    async createTrip(passID, starting_trip) {
+        if (this._isLogQuery) console.info(this.successColor, 'Creating Trip with passengerID: ' + passID)
 
         var newTrip = await addDoc(
             collection(this._db, 'Trips'),
@@ -82,7 +120,7 @@ class TripRepository {
     }
 
     async setTripDriver(tripID, driverID, driver_to_pick_up_status) {
-        if (this._isLogQuery) console.log('Setting Trip '+ tripID + ' to have driver: ' + driverID)
+        if (this._isLogQuery) console.info(this.successColor, 'Setting Trip '+ tripID + ' to have driver: ' + driverID)
 
         return await updateDoc(
             doc(this._db, 'Trips/'+tripID),
@@ -94,7 +132,7 @@ class TripRepository {
     }
 
     async updateTripStatus(tripID, new_status) {
-        if (this._isLogQuery) console.log('Setting Trip '+ tripID + ' to have status: ' + new_status)
+        if (this._isLogQuery) console.info(this.successColor, 'Setting Trip '+ tripID + ' to have status: ' + new_status)
 
         return await updateDoc(
             doc(this._db, 'Trips/'+tripID),
@@ -105,7 +143,7 @@ class TripRepository {
     }
     
     async endTrip(tripID, att_updating) {
-        if (this._isLogQuery) console.log('Ending Trip: '+ tripID)
+        if (this._isLogQuery) console.info(this.successColor, 'Ending Trip: '+ tripID)
 
         return await updateDoc(
             doc(this._db, 'Trips/'+tripID),
@@ -114,7 +152,7 @@ class TripRepository {
     }
 
     async setTripDriverRating(tripID, rating) {
-        if (this._isLogQuery) console.log('Setting Driver Rating for Trip: '+ tripID + ' Rating: ' + rating)
+        if (this._isLogQuery) console.info(this.successColor, 'Setting Driver Rating for Trip: '+ tripID + ' Rating: ' + rating)
 
         return await updateDoc(
             doc(this._db, 'Trips/'+tripID),
@@ -125,7 +163,7 @@ class TripRepository {
     }
 
     async setTripPassengerRating(tripID, rating) {
-        if (this._isLogQuery) console.log('Setting Passenger Rating for Trip: '+ tripID + ' Rating: ' + rating)
+        if (this._isLogQuery) console.info(this.successColor, 'Setting Passenger Rating for Trip: '+ tripID + ' Rating: ' + rating)
 
         return await updateDoc(
             doc(this._db, 'Trips/'+tripID),
@@ -136,21 +174,21 @@ class TripRepository {
     }
 
     async deleteTrip(tripID) {
-        if (this._isLogQuery) console.log('Deleting Trip with tripID: ' + tripID)
+        if (this._isLogQuery) console.info(this.successColor, 'Deleting Trip with tripID: ' + tripID)
 
         var response = "Success"
         await deleteDoc( 
             doc(collection(this._db, 'Trips'), tripID)
         )
         .catch(function(error) {
-            console.log("Remove failed: " + error.message)
+            console.info("Remove failed: " + error.message)
             response = "Failed"
         });
         return response
     }
 
     async setUltimateAttr(tripID, attributes) {
-        if (this._isLogQuery) console.log('Setting 1+ attributes for Trip: '+ tripID)
+        if (this._isLogQuery) console.info(this.successColor, 'Setting 1+ attributes for Trip: '+ tripID)
 
         return await updateDoc(
             doc(collection(this._db, 'Trips'), tripID), attributes).then(doc=>console.error(attributes));
